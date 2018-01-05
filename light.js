03c4c8ad821ea59004869cfc1f19b85675fda99f
agron911 <harrison0911@gmail.com>
2018-01-05 23:30:35 +0800

    'light'

--- light.js
+++ light.js
@@ -1,44 +1,102 @@
-var led1;
-var led2;
-var timer;
+var firebase;
+var blue;
+var red;
+var config = {
+  databaseURL: "https://chatbot-firebase-c52b9.firebaseio.com/"
+};
 
+firebase.initializeApp(config);
+var database = firebase.database().ref();
 
-boardReady({device: ''}, function (board) {
+console.log('裝置連線中...');
+
+boardReady({device: 'Rml1'}, function(board) {
   board.systemReset();
-  board.samplingInterval = 20;
-  led1 = getLed(board, 10);
-  led2 = getLed(board, 11);
-  document.getElementById("demo-area-02-light").addEventListener("click", function(){
-    if (document.getElementById("demo-area-02-light").className == "off") {
-      document.getElementById("demo-area-02-light").className = "on";
-      var repeat = function(){
-        var time;
-        var repeatDelay = function(time){
-          return new Promise(function(resolve){
-            timer = setTimeout(resolve,time);
+  board.samplingInterval = 250;
+  blue = getLed(board, 10);
+  red = getLed(board, 7);
+  database.push({
+    name: '智慧燈泡',
+    content: '我上線囉！',
+    time: getTime()
+  });
+
+  var event = [
+    {
+     text:'開燈',
+     msg:'燈泡已經打開！',
+     fn:function(){blue.on();},
+     
+    }, {
+     text:'開燈',
+   
+     fn:function(){red.off();},
+    },
+    {
+     text:'關燈',
+     msg:'燈泡已經關起來了！',
+     fn:function(){blue.off();}
+    }, {
+     text:'關燈',
+     fn:function(){red.on();}
+    },
+    {
+     text:'全關',
+     msg:'close it all！',
+     fn:function(){blue.off();}
+    },
+    {
+     text:'全關',
+     fn:function(){red.off();}
+    },
+    {
+     text:'閃爍',
+     msg:'一閃一閃亮晶晶~',
+     fn:function(){blue.blink(500);}
+    },     {
+     text:'閃爍',
+    
+     fn:function(){red.blink(1000);}
+    }
+    
+  ];
+
+
+  database.limitToLast(1).on('value', function(snapshot) {
+    var s = {};
+    for(var i in snapshot.val()){
+      s = snapshot.val()[i];
+      console.log('('+s.time+') '+s.name+' 說：'+s.content);
+      for(var j in event){
+        if(s.content.indexOf(event[j].text)!=-1){
+          
+          event[j].fn();
+          database.push({
+            name: '智慧燈泡',
+            content:event[j].msg,
+            time: getTime()
           });
-        };
-        var repeatPromise = function(){
-          repeatDelay(1).then(function(){
-              led1.on();
-        led2.off();
-            return repeatDelay(500);
-          }).then(function(){
-              led1.off();
-        led2.on();
-            return repeatDelay(500);
-          }).then(function(){
-              repeatPromise();
-          });
-        };
-        repeatPromise();
-      };
-      repeat();
-    } else {
-      document.getElementById("demo-area-02-light").className = "off";
-      clearTimeout(timer);
-      led1.off();
-      led2.off();
+        }
+      }     
     }
   });
 });
+
+
+function getTime() {
+  var date = new Date();
+  var h = date.getHours();
+  var m = date.getMinutes();
+  var s = date.getSeconds();
+  if (h < 10) {
+    h = '0' + h;
+  }
+  if (m < 10) {
+    m = '0' + m;
+  }
+  if (s < 10) {
+    s = '0' + s;
+  }
+  var now = h + ':' + m + ':' + s;
+  return now;
+}
